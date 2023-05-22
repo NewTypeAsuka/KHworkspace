d02d9b8c6dcd2c7a3667d443dbbb922637ebd11b
sangzoon0102 <sangzoon0102@naver.com>
2023-05-22 10:20:04 +0900

    20230522_과제제출후오류잡음

--- 06_Framework/boardProject/src/main/java/edu/kh/project/common/scheduling/ImageDeleteScheduling.java
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/common/scheduling/ImageDeleteScheduling.java
@@ -40,50 +40,50 @@ public class ImageDeleteScheduling {
 		System.out.println("스케줄러가 10초마다 자동으로 출력");
 	}
 	
-	// - cron 속성 : UNIX계열 잡 스케쥴러 표현식으로 작성 - cron="초 분 시 일 월 요일 [년도]" - 요일 : 1(SUN) ~ 7(SAT) 
-//	@Scheduled(cron = "0,30 * * * * * ") // -> 매분 0초, 30초 실행한다는 뜻
-	@Scheduled(cron = "0 0 * * * * * ") // -> 매 정시마다 실행한다는 뜻( *시 0분 0초 )
-	public void text3() {
-//		System.out.println("스케줄러가 x분 0초, x분 30초마다 자동으로 출력");
-		
-		System.out.println("-----게시판 DB, 서버 불일치 파일 제거-----");
-		// 서버에 저장된 파일 목록을 조회해서 DB에 저장된 파일 목록과 비교하여 매칭되지 않는 서버 파일 제거
-		
-		// 1. 서버에 저장된 파일 목록 조회 -> application 객체를 이용해서 /resources/images/board의 실제 서버 경로를 얻어옴
-		String filePath = servletContext.getRealPath("/resources/images/board");
-		
-		// 2. filePath에 저장된 모든 파일 목록 읽어오기
-		File path = new File(filePath);
-		File[] imageArr = path.listFiles();
-		
-		// 배열 -> List로 변환
-		List<File> serverImageList = Arrays.asList(imageArr);
-		
-		// 3. DB 파일 목록 조회
-		List<String> dbImageList = service.selectImageList();
-		
-		// 4. 서버에 파일 목록이 있을 경우에 비교 시작
-		if(!serverImageList.isEmpty()) {
-			
-			// 5. 서버 파일 목록을 순차 접근
-			for(File server : serverImageList) {
-				
-				// 6. 서버에 존재하는 파일이 DB(dbImageList)에 없다면 삭제
-//				String[] temp = server.toString().split("\\");
-//				String s = temp[temp.length-1];
+//	// - cron 속성 : UNIX계열 잡 스케쥴러 표현식으로 작성 - cron="초 분 시 일 월 요일 [년도]" - 요일 : 1(SUN) ~ 7(SAT) 
+////	@Scheduled(cron = "0,30 * * * * * ") // -> 매분 0초, 30초 실행한다는 뜻
+//	@Scheduled(cron = "0 0 * * * * * ") // -> 매 정시마다 실행한다는 뜻( *시 0분 0초 )
+//	public void text3() {
+////		System.out.println("스케줄러가 x분 0초, x분 30초마다 자동으로 출력");
+//		
+//		System.out.println("-----게시판 DB, 서버 불일치 파일 제거-----");
+//		// 서버에 저장된 파일 목록을 조회해서 DB에 저장된 파일 목록과 비교하여 매칭되지 않는 서버 파일 제거
+//		
+//		// 1. 서버에 저장된 파일 목록 조회 -> application 객체를 이용해서 /resources/images/board의 실제 서버 경로를 얻어옴
+//		String filePath = servletContext.getRealPath("/resources/images/board");
+//		
+//		// 2. filePath에 저장된 모든 파일 목록 읽어오기
+//		File path = new File(filePath);
+//		File[] imageArr = path.listFiles();
+//		
+//		// 배열 -> List로 변환
+//		List<File> serverImageList = Arrays.asList(imageArr);
+//		
+//		// 3. DB 파일 목록 조회
+//		List<String> dbImageList = service.selectImageList();
+//		
+//		// 4. 서버에 파일 목록이 있을 경우에 비교 시작
+//		if(!serverImageList.isEmpty()) {
+//			
+//			// 5. 서버 파일 목록을 순차 접근
+//			for(File server : serverImageList) {
 //				
-//				System.out.println(server.getName());
-				
-				// List.indexOf(객체) = 객체가 List에 있으면 해당 인덱스 반환, 없으면 -1 반환
-				if(dbImageList.indexOf(server.getName()) == -1) { // 서버 파일 이름에 있는 게 db파일 목록에 없는 경우
-					
-					System.out.println(server.getName() + " 삭제");
-					server.delete(); // 삭제
-				}
-			} // for문 종료
-			System.out.println("-----이미지 파일 삭제 스케줄러 종료-----");
-		}
-	}
+//				// 6. 서버에 존재하는 파일이 DB(dbImageList)에 없다면 삭제
+////				String[] temp = server.toString().split("\\");
+////				String s = temp[temp.length-1];
+////				
+////				System.out.println(server.getName());
+//				
+//				// List.indexOf(객체) = 객체가 List에 있으면 해당 인덱스 반환, 없으면 -1 반환
+//				if(dbImageList.indexOf(server.getName()) == -1) { // 서버 파일 이름에 있는 게 db파일 목록에 없는 경우
+//					
+//					System.out.println(server.getName() + " 삭제");
+//					server.delete(); // 삭제
+//				}
+//			} // for문 종료
+//			System.out.println("-----이미지 파일 삭제 스케줄러 종료-----");
+//		}
+//	}
 }
 
 /*
