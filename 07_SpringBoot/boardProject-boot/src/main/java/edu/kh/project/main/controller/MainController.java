7b8cd9fff179b635f620a7ac299487f9c60cc03a
sangzoon0102 <sangzoon0102@naver.com>
2023-06-09 12:50:08 +0900

    20230609_스프링부트

--- 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -2,7 +2,9 @@ package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -16,5 +18,13 @@ public class MainController {
 		// SpringBoot의 경우(+thymeleaf 템플릿 엔진 적용 == jsp 대신 사용)
 		// src/main/resources/templates/common/main.html로 forward
 		return "common/main";
+	}
+	
+	@GetMapping("/loginError")
+	public String loginError(RedirectAttributes ra) {
+		
+		ra.addFlashAttribute("message", "로그인 후 이용해주세요.");
+		
+		return "redirect:/";
 	}
 }
