631b7e6afe0c5dc0abe7587bae8ff7d6020015bf
sangzoon0102 <sangzoon0102@naver.com>
2023-06-22 10:18:35 +0900

    스프링부트_채팅

--- 06_Framework/boardProject/src/main/java/edu/kh/project/common/aop/CommonPointcut.java
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/common/aop/CommonPointcut.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.annotation.Pointcut;
 public class CommonPointcut {
 
 	@Pointcut("execution(* edu.kh.project..*Impl*.*(..))")
-	public void serviceImplePointcut() {
+	public void serviceImplPointcut() {
 		
 	}
 }
