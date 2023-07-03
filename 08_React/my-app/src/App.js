58644825d545f92e5a709c8ff1b68ae80425e542
sangzoon0102 <sangzoon0102@naver.com>
2023-07-03 11:15:23 +0900

    스프링부트 state

--- 08_React/my-app/src/App.js
+++ 08_React/my-app/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Ex1 from './components/Exam1'; // 동일한 폴더 내에 있는(./) components 폴더의 Exam1.js를 가져와 Ex1이란 이름으로 사용
 import Ex2 from './components/Exam2'; // 동일한 폴더 내에 있는(./) components 폴더의 Exam2.js를 가져와 Ex2이란 이름으로 사용
 import PropsEx from './components/R01_props1';
+import State1 from './components/R02_state1';
+import State2 from './components/R03_state2';
+import State3 from './components/R04_state3';
 
 function App() {
   // 리액트의 컴포넌트는 딱 하나의 요소만을 반환할 수 있음
@@ -13,14 +16,23 @@ function App() {
       {/* jsx 주석 */}
       <h1>Hello React!</h1>
       <div>Wow! It's React!</div>
+      <hr/>
 
       {/* <Ex1/> */}
       {/* <Ex2/> */}
-      <PropsEx name={'홍길동'}/>
-      <PropsEx name={'성춘향'}/>
-      <PropsEx name={'이순신'}/>
-      
+      {/* <PropsEx name={'홍길동'}/> */}
+      {/* <PropsEx name={'성춘향'}/> */}
+      {/* <PropsEx name={'이순신'}/> */}
+      <hr/>
+      <State1/>
+      <hr/>
+      <State2 init={100}/>
+      <hr/>
+      <State3/>
+      <hr/>
+
     </>
+    
   );
 }
 
---
+++ 08_React/my-app/src/components/R02_state1.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+// 리액트는 컴포넌트의 상태가 변할 때마다 리렌더링 수행
+const InputTest = () => {
+
+    //        변수          함수
+    const [inputValue, setInputValue] = useState('초기값');
+    // inputValue: 값을 저장하는 변수
+    // setInputValue: inputValue에 값을 대입하는 setter 역할의 함수
+
+    const changeInputValue = (e) => {
+
+        console.log(e.target.value);
+        setInputValue(e.target.value);
+    }
+
+    return (
+
+        // 첫 렌더링: value="초기값"
+        // -> input의 값을 변경(컴포넌트의 상태 변화 -> 리렌더링 진행)
+        //     + onChange(값이 변했을 때) -> changeInputValue 함수 실행
+        //      -> inputValue에 e.target.value(변화된 값) 대입
+        <input type="text" value={inputValue}
+        
+            // onChange={ (e) => {setInputValue(e.target.value)} }/>
+            onChange={changeInputValue}/>
+
+    );
+};
+
+export default InputTest;
